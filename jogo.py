3b3cacb4138d3e357d1e293e7c9f77500184acdb
batistasantosevelin-droid <batista.santos.evelin@escola.pr.gov.br>
2026-08-24 14:55:13 +0000

    adicionado codiggo do jogo

--- jogo.py
+++ jogo.py
@@ -1,1 +1,20 @@
+)
 
+botao_iniciar.pack(pady=10)
+
+
+botao_tentar = tk.Button(
+janela,
+text="TENTAR",
+command=verificar_palpite,
+font=("Arial", 12)
+)
+
+botao_tentar.pack(pady=5)
+
+
+# -----------------------------
+# MANTÉM A JANELA ABERTA
+# -----------------------------
+
+janela.mainloop()
