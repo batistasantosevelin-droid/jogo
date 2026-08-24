533cbb99d24d8af1f350945706a195462d20914c
batistasantosevelin-droid <batista.santos.evelin@escola.pr.gov.br>
2026-08-24 15:27:40 +0000

    codigo do jogo digitado

--- jogo.py
+++ jogo.py
@@ -1,4 +1,77 @@
+import tkinter as tk
+import random
+
+numero_secreto = 0
+tentativas = 0
+limiti = 20
+
+janela = tk.TK()
+
+janela.title("JOGO DE ADIVINHAÇÃO")
+janela.geometry("450x500")
+
+titulo = tk.Label(
+janela,
+text="JOGO ADIVINHAÇÃO",
+font=("Arial", 20)
 )
+
+titulo.pack(pady=20)
+
+campo_nome =tk.Entry(
+janela,
+font=("Arial", 14)
+)
+
+campo_nome.pack(pady=5)
+
+texto_dificuldade =tk.Label(
+janela,
+text="ESCOLHA A DIFICULDADE"
+)
+
+texto_dificuldade.pack(pady=10)
+
+DIFICULDADE =tk.StringVar()
+
+dificuldade.set("Facil")
+
+menu_dificuldade=tk.OpitionMenu(
+janela,
+dificuldade,
+"Fcail"
+"Medio"
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 botao_iniciar.pack(pady=10)
 
